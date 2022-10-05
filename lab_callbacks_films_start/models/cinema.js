d1f9ae14b4de38982b29d931bbfec7723cc5c60c
Cammy Smith <cjsarab1991@gmail.com>
2022-10-05 11:49:18 +0100

    Second test

--- lab_callbacks_films_start/models/cinema.js
+++ lab_callbacks_films_start/models/cinema.js
@@ -2,10 +2,22 @@ const Cinema = function (films) {
   this.films = films;
 };
 
-
-
-Cinema.prototype.filmTitles = function(){
+Cinema.prototype.filmTitles = function() {
   return result = this.films.map(film=>film.title);
 };
+
+Cinema.prototype.findFilmByTitle = function(title) {
+  let result = this.films.filter((film)=>{return title})
+  return result;
+}
+
+
+
+
+
+
+
+
+
 
 module.exports = Cinema;
